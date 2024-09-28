4f9d845f4312e888b9730ac5841901616125ea4e
omriloewen <loewenbach@mail.tau.ac.il>
2024-09-28 22:08:15 +0300

    fix precfile

--- c_extensions/kmeansppmodule/setup.py
+++ c_extensions/kmeansppmodule/setup.py
@@ -1,6 +1,8 @@
 from setuptools import setup, Extension
 
-module = Extension("mykmeanssp", sources=["kmeansppmodule.c"])
+module = Extension(
+    "mykmeanssp", sources=["c_extensions/kmeansppmodule/kmeansppmodule.c"]
+)
 setup(
     name="mykmeanssp",
     version="1.0",
